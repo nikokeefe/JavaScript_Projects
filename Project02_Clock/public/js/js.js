d217ad63e080ce365aeeae94e0634d154cc24a95
Nik O'Keefe <nikwillok@gmail.com>
2019-01-13 22:52:27 +1300

    fine adjustments to clock :)

--- Project02_Clock/public/js/js.js
+++ Project02_Clock/public/js/js.js
@@ -11,22 +11,23 @@ const hourHand = document.querySelector('.hour-hand'); // grab our hour-hand
 function setDate() {
 
     const now = new Date();
+    
+    console.log(now);
 
     const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60) * 360) + 90; // add 90deg so defauulis up and down
+    const secondsDegrees = ((seconds / 60) * 360) + 90; // add 90deg so defaults to up and down
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
     
     const minutes = now.getMinutes();
     const minutesDegrees = ((minutes / 60) * 360) + 90; 
     minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
-    const hours = now.getMinutes();
+    const hours = now.getHours();
     const hoursDegrees = ((hours / 12) * 360) + 90; 
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 
 }
 
 setInterval(setDate, 1000); // set our 1 sec intervals
-
 
 setDate();
