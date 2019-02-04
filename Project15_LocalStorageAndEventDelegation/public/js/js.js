37c1783df7661427a484ebbb76c31ca04bdb1373
Nik O'Keefe <nikwillok@gmail.com>
2019-02-04 22:35:07 +1300

    Project15, sorted added a few new buttons, just for the challenge 😲

--- Project15_LocalStorageAndEventDelegation/public/js/js.js
+++ Project15_LocalStorageAndEventDelegation/public/js/js.js
@@ -1,3 +1,95 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = [];
+const checkAll = document.querySelector('.check-all');
+const uncheckAll = document.querySelector('.uncheck-all');
+const deleteAll = document.querySelector('.delete-all');
+const ul = document.querySelector('ul');
+let items = JSON.parse(localStorage.getItem('items')) || [];
+
+function addItem(e) {
+    
+    e.preventDefault();
+    
+    const text = (this.querySelector('[name=item')).value;
+    const item = {
+        text, // ES6 same as 'text: text'
+        done: false
+    };
+    
+    items.push(item);
+    populateList(items, itemsList)
+    
+    localStorage.setItem('items', JSON.stringify(items));
+    
+    this.reset();
+}
+
+function populateList(plates = [], platesList) {
+    platesList.innerHTML = plates.map((plate, i) => {
+        return `
+        <li>
+            <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
+            <label for="item${i}">${plate.text}</label>
+        </li>
+        `;
+    }).join(''); // joins it all together for html string
+}
+
+function toggleDone(e) {
+
+    if (!e.target.matches('input')) return; //skip this unless its an input
+    const element = e.target; //get the input
+    const index = element.dataset.index; // get the data index
+    items[index].done = !items[index].done; // set to the opposite of what it currently is
+    
+    localStorage.setItem('items', JSON.stringify(items)); //save it 
+    populateList(items, itemsList); // display it
+}
+
+
+function checkAllBoxes() {
+    
+    items.forEach(element => {
+        element.done = true;
+      });
+      
+      populateList(items, itemsList);
+
+}
+
+function uncheckAllBoxes() {
+
+    items.forEach(element => {
+      element.done = false;
+    });
+    
+    populateList(items, itemsList);
+    
+}
+
+function deleteAllPlates() {
+
+    localStorage.clear(); 
+
+    while (ul.firstChild) {
+        ul.removeChild(ul.firstChild);
+    }
+
+    items = [];
+
+    console.log(items)
+}
+
+
+
+addItems.addEventListener('submit', addItem);
+
+itemsList.addEventListener('click', toggleDone);
+
+checkAll.addEventListener('click', checkAllBoxes);
+
+uncheckAll.addEventListener('click', uncheckAllBoxes);
+
+deleteAll.addEventListener('click', deleteAllPlates);
+
+populateList(items, itemsList);
